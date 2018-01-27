5f66d7967f29bea29d9554cebd7402ffece05aa5
Amit Singh Sansoya <tusharamit@yahoo.com>
2018-01-28 03:21:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $rr=exec('python wer.py');
 echo "This is the first application\n";
 echo $rr;
-$ew=shell_exec('python FaceRecognition1.py');
+$ew=shell_exec('python face.py');
 echo "\nThis is the first application\n";
 echo "\n$ew";
 ?>
